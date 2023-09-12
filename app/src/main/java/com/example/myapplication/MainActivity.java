66e107f20fdbdf36404303a36fc642d1ad59d8fd
EduardoAlbert <albert-costa1@live.com>
2023-09-12 13:34:52 -0300

    Exercise 1 and 2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +18,26 @@ public class MainActivity extends AppCompatActivity {
     public void trocaTexto(View view) {
         TextView texto = findViewById(R.id.textViewTexto);
 
-        texto.setText("Mudei o texto");
+        if (texto.getText().equals("Texto original")) {
+            texto.setText("Mudei o texto");
+            return;
+        }
+
+        texto.setText("Texto original");
+
+    }
+
+    public void soma(View view) {
+        TextView resultado = findViewById(R.id.textViewResultado);
+        EditText numero1 = findViewById(R.id.editTextN1);
+        EditText numero2 = findViewById(R.id.editTextN2);
+
+        double n1, n2, res;
+        n1 = Integer.parseInt(numero1.getText().toString());
+        n2 = Integer.parseInt(numero2.getText().toString());
+
+        res = n1 + n2;
+        resultado.setText(""+res);
+
     }
 }
